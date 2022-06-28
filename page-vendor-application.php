c8c15057e7fbfaab58980df192650f27992fbb79
Zhaorui Xu <xzr0429@gmail.com>
2022-06-28 13:44:36 -0700

    update functions.php and create page-vendor-application.php

---
+++ page-vendor-application.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The template for displaying vendor application page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Food_Expo
+ */
+acf_form_head();
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+      get_template_part( 'template-parts/featured-vendors' );
+
+      ?>
+      <div id="content">
+	
+      <?php
+      // To create a new vendor post (typeof ife-vendor CPT)
+      acf_form(array(
+        'post_id'		=> 'new_post',
+        'post_title'	=> true,
+        'post_content'	=> false,
+        'new_post'		=> array(
+          'post_type'		=> 'ife-vendor',
+          'post_status'	=> 'pending'
+        ),
+        'return'		=> home_url('vendor-thank-you'), // redirect to thank-you page
+        'submit_value'	=> 'Send'
+      ));
+      
+      ?>
+      
+    </div>
+
+    <?php  
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+// get_sidebar();
+get_footer();
