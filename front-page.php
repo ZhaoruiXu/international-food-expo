e0a4740bc5162561c166212c22abf629159e17ba
cljang <clayton.jang@gmail.com>
2022-06-28 11:01:50 -0700

    Add event dates to home page and start google map implementation

--- front-page.php
+++ front-page.php
@@ -28,17 +28,53 @@ get_header();
 				<div class="left-column">
 					<?php
 					// ID: 60 - About Page
-					if ( function_exists( 'get_field' ) ) {
-						if ( get_field( 'description', 60 ) ) {
+					if ( function_exists( 'get_field' ) ) :
+						if ( get_field( 'description', 60 ) ) :
 							the_field( 'description', 60 );
-						}
-					}
+						endif;
+					endif;
 					?>
 					
-					<a href=<?php echo get_page_link(60) ?>>More Info <span class="screen-reader-text">About the Convention</span></a>
+					<a href=<?php echo get_page_link(60) ?> class="about-page-link" >More Info <span class="screen-reader-text">About the Convention</span></a>
 				</div>
 				<div class="right-column">
+					<?php 
+					// ID: 60 - About Page
+					if ( function_exists( 'get_field' ) ) :
+						if( get_field('event_address', 60) ): 
+							// $location = get_field('event_address', 60);
+							// echo $location['name'];
+							?>
+							<!-- <div class="acf-map" data-zoom="16">
+									<div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
+							</div> -->
+							<?php 
+						endif; 
+						
+						// Event Dates
+						if( have_rows('event_dates', 60) ):
+							?>
+							<section class="event-dates">
+								<h3>Event Dates</h3>
+								<ul class="date-list">
+									<?php
+									while( have_rows('event_dates', 60) ) : 
+										the_row();
 
+										// Format as Day of Week, Month Day Year
+										$date = date( 'l, F j Y', strtotime( get_sub_field('date') ) );
+
+										?>
+										<li><?php echo $date ?></li>
+										<?php
+									endwhile;
+									?>
+								</ul>
+							</section>
+							<?php
+						endif;
+					endif;
+					?>
 				</div>
 			</section>
 			<?php
