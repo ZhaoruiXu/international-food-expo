41a3367a852af5abc76a52fd82f7e978919c02f4
Zhaorui Xu <xzr0429@gmail.com>
2022-06-28 21:18:57 -0700

    add page-about.php

---
+++ page-about.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * The template for displaying about page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Food_Expo
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			// get_template_part( 'template-parts/content', 'page' );
+
+      if ( function_exists ( 'get_field' ) ) {
+
+            if ( get_field( 'description' ) ) {
+            ?>
+
+                <section class="about-description">
+                  <?php the_title( '<h2>', '</h2>' ); ?>
+                  <?php the_field( 'description' ); ?>
+                </section>
+
+            <?php 
+            }
+            ?>  
+
+           <section class="about-history">
+           <?php  
+           if( have_rows('history') ):
+                // Loop through rows.
+                ?>
+
+                <div class="history-year">
+
+                <?php  
+                while( have_rows('history') ) : the_row();
+
+                    // Load sub field value.
+                    if( get_sub_field('year') ){
+                      $sub_value_year = get_sub_field('year');
+                    ?>
+                      <h3><?php echo $sub_value_year ?></h3>
+                    <?php    
+                    }
+
+                // End loop.
+                endwhile;
+                ?>
+
+                </div>
+
+                <div class="history-description">
+                
+                <?php 
+                while( have_rows('history') ) : the_row();
+
+                    // Load sub field value.
+                    if( get_sub_field('description') ){
+                      $sub_value_year = get_sub_field('description');
+                    ?>
+                      <h3><?php echo $sub_value_year ?></h3>
+                    <?php    
+                    }
+
+                // End loop.
+                endwhile;
+                ?>
+
+                </div>
+
+            <?php 
+            endif;
+            ?>
+            </section> 
+            
+            <section class="about-organizers">
+            <?php     
+            if( have_rows('organizers') ):
+                // Loop through rows.
+                ?>
+
+                <?php  
+                while( have_rows('organizers') ) : the_row();
+
+                    // Load sub field value.
+                    $sub_value_name = get_sub_field('name');
+                    $sub_value_description = get_sub_field('description');
+                    $sub_value_image = get_sub_field('image');
+                    ?>
+
+                    <div class="about-organizer">
+                    <h3><?php echo $sub_value_name ?></h3>
+                    <p><?php echo $sub_value_description ?></p>
+                    <?php  
+                    echo wp_get_attachment_image( $sub_value_image, 'thumbnail' );
+                    ?>
+                    </div>
+                    <?php  
+
+                // End loop.
+                endwhile;
+                ?>
+
+            <?php 
+            endif;
+            ?>
+            </section>
+
+            <section class="about-location-time">
+
+            </section>
+
+            <?php  
+        } 
+
+      get_template_part( 'template-parts/featured-vendors' );
+
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
