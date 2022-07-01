1523cfe785c0e20f8ea310fb0b38617e2b34e682
cljang <clayton.jang@gmail.com>
2022-06-30 23:34:22 -0700

    Update single-ife page

--- single-ife-event.php
+++ single-ife-event.php
@@ -12,59 +12,65 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-	<?php get_template_part( 'template-parts/banner' ); ?>
-	<p><?php the_field('date'); ?></p>
-
-
-	<article>
-
-	<div class = 'events'>
-
+		
 		<?php
 		while ( have_posts() ) :
 			the_post();
+			get_template_part( 'template-parts/banner' ); 
 			
-		
-          	if ( function_exists('get_field' ) ) {
-
-                if ( get_field( 'description' ) ) {
-					echo "<p>". get_field( 'description'). "</p>";
+			?>
+			<section class="event-details">
+				<?php
+				if ( function_exists('get_field' ) ) {
+					if ( get_field( 'time' ) ) {
+						?>
+						<p class="event-time"><?php the_field('time'); ?></p>
+						<?php
+					}
+					if ( get_field( 'description' ) ) {
+						?>
+						<div class="event-description">
+							<?php the_field('description'); ?>
+						</div>
+						<?php
+					}
 				}
-            }
+				?>
+			</section>
+			<?php
 
 			if( have_rows('guests') ):
-
-				echo '<h2> Featured Guests</h2>';
-				// loop through the rows of data
-			   while ( have_rows('guests') ) : the_row();
-		   
-				   // display a sub field value
-				   the_sub_field('name');
-				   $image = get_sub_field('image');
-				   echo wp_get_attachment_image($image,'full');
-				   the_sub_field('description');
-					
-			   endwhile;
-			   
-			endif;
-			
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
+				?>
+				<section class="featured-guests">
+					<h2>Featured Guests</h2>
+					<div class="guest-wrapper">
+						<?php
+							// loop through the rows of data
+							while ( have_rows('guests') ) : 
+								the_row();
+								?>
+								<article class="guest">
+									<h3 class="guest-name"><?php the_sub_field('name') ?></h3>
+									<?php
+									$image = get_sub_field('image');
+									echo wp_get_attachment_image($image,'full');
+									?>
+									<p class="guest-description"><?php the_sub_field('description') ?></p>
+								</article>
+								<?php
+							endwhile;
+						?>
+					</div>
+				</section>
+				<?php
 			endif;
 
 		endwhile; // End of the loop.
 		?>
 		
 		<?php get_template_part('template-parts/featured-vendors');?>
-		
-	</div>
-	</article>
 	</main><!-- #main -->
 
 <?php
 
-
-get_sidebar();
 get_footer();
