8b3e30b75daa62e72cd3ab9f23a639099b674e3a
cljang <clayton.jang@gmail.com>
2022-07-05 11:26:03 -0700

    Style banner section

--- template-parts/banner.php
+++ template-parts/banner.php
@@ -1,8 +1,8 @@
 <?php 
 ?>
-<header class="banner-section">
-  <h1><?php the_title() ?></h1>
+<header class="banner-part">
   <?php the_post_thumbnail('ife-banner') ?>
+  <h1><?php the_title() ?></h1>
 </header>
 
 <?php
