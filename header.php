06483f5d6e0f1cada9502e9a60d8d3b83af2b553
cljang <clayton.jang@gmail.com>
2022-07-10 16:21:05 -0700

    Make logo in an h1 tag on front page

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
 						bloginfo( 'name' );
 					endif;
 				}
-				if ( is_front_page() && is_home() ) :
+				if ( is_front_page() ) :
 					?>
 					<h1 class="site-title">
 						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
