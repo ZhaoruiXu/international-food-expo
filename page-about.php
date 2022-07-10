083a34df62fbf6b6cad741612bf539dd4435862c
cljang <clayton.jang@gmail.com>
2022-07-10 00:33:13 -0700

    Make use-blue-section mixin more intuitive

--- page-about.php
+++ page-about.php
@@ -39,6 +39,48 @@ get_header();
           }
           ?>  
         </section>
+            
+        <section class="about-organizers">
+          <?php     
+          if( have_rows('organizers') ):
+              // Loop through rows.
+              ?>
+              <h2>Organizers</h2>
+              <div class="carousel">
+                <button class="swiper-button-prev swiper-organizers-button-prev"></button>
+                <div class="swiper swiper-organizers">
+                  <div class="swiper-wrapper">
+                    <?php
+                    while( have_rows('organizers') ) : the_row();
+                      // Load sub field value.
+                      if(get_sub_field('name') && get_sub_field('description') && get_sub_field('image')){
+                        $sub_value_name = get_sub_field('name');
+                        $sub_value_description = get_sub_field('description');
+                        $sub_value_image = get_sub_field('image');
+                        ?>
+                        <div class=swiper-slide>
+                          <article class="organizer">
+                            <?php
+                            echo wp_get_attachment_image( $sub_value_image, 'thumbnail' );
+                            ?>
+                            <h3><?php echo $sub_value_name ?></h3>
+                            <p><?php echo $sub_value_description ?></p>
+                          </article>
+                        </div>
+                        <?php
+                      }
+                    // End loop.
+                    endwhile;
+                    ?>
+                </div>
+              </div>
+              <button class="swiper-button-next swiper-organizers-button-next"></button>
+              <nav class="swiper-pagination  swiper-organizers-pagination"></nav>
+              </div>
+          <?php 
+          endif;
+          ?>
+        </section>
 
         <section class="about-history">
           <?php 
@@ -104,48 +146,6 @@ get_header();
           endif;
           ?>
         </section> 
-            
-        <section class="about-organizers">
-          <?php     
-          if( have_rows('organizers') ):
-              // Loop through rows.
-              ?>
-              <h2>Organizers</h2>
-              <div class="carousel">
-                <button class="swiper-button-prev swiper-organizers-button-prev"></button>
-                <div class="swiper swiper-organizers">
-                  <div class="swiper-wrapper">
-                    <?php
-                    while( have_rows('organizers') ) : the_row();
-                      // Load sub field value.
-                      if(get_sub_field('name') && get_sub_field('description') && get_sub_field('image')){
-                        $sub_value_name = get_sub_field('name');
-                        $sub_value_description = get_sub_field('description');
-                        $sub_value_image = get_sub_field('image');
-                        ?>
-                        <div class=swiper-slide>
-                          <article class="organizer">
-                            <?php
-                            echo wp_get_attachment_image( $sub_value_image, 'thumbnail' );
-                            ?>
-                            <h3><?php echo $sub_value_name ?></h3>
-                            <p><?php echo $sub_value_description ?></p>
-                          </article>
-                        </div>
-                        <?php
-                      }
-                    // End loop.
-                    endwhile;
-                    ?>
-                </div>
-              </div>
-              <button class="swiper-button-next swiper-organizers-button-next"></button>
-              <nav class="swiper-pagination  swiper-organizers-pagination"></nav>
-              </div>
-          <?php 
-          endif;
-          ?>
-        </section>
           
         <section class="about-location-time">
           
