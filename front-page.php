9626ddd5ce327855d7db6fbee0b1945143906b67
Zhaorui Xu <xzr0429@gmail.com>
2022-07-11 15:42:52 -0700

    finish home page scss

--- front-page.php
+++ front-page.php
@@ -78,74 +78,82 @@ get_header();
 			
 			?>
 			<section class="section-about">
-				<div class="left-column">
-					<?php
-					// ID: 60 - About Page
-					if ( function_exists( 'get_field' ) ) :
-						if ( get_field( 'description', 60 ) ) :
-							the_field( 'description', 60 );
+				<div class="section-about-wrapper">
+					<div class="left-column">
+						<h2>About</h2>
+						<?php
+						// ID: 60 - About Page
+						if ( function_exists( 'get_field' ) ) :
+							if ( get_field( 'description', 60 ) ) :
+								the_field( 'description', 60 );
+							endif;
 						endif;
-					endif;
-					?>
-					
-					<a href=<?php echo get_page_link(60) ?> class="about-page-link" >More Info <span class="screen-reader-text">About the Convention</span></a>
-				</div>
-				<div class="right-column">
-					<?php 
-					get_template_part( 'template-parts/event-map' );
+						?>
+						
+						<a href=<?php echo get_page_link(60) ?> class="about-page-link" >More Info <span class="screen-reader-text">About the Convention</span></a>
+					</div>
+					<div class="right-column">
+						<?php 
+						get_template_part( 'template-parts/event-map' );
+						?>
+						<div class="right-column-content">
+							<?php  
+							if ( function_exists( 'get_field' ) ) :
+								// Event Dates
+								if ( get_field( 'start_date', 60 ) && get_field( 'end_date', 60 ) ) {
+									?>
+									<p class="event-dates"><?php the_field( 'start_date', 60 )?> - <?php the_field( 'end_date', 60 )?></p>
+									<?php
+								}
 
-					if ( function_exists( 'get_field' ) ) :
-						// Event Dates
-            if ( get_field( 'start_date', 60 ) && get_field( 'end_date', 60 ) ) {
-              ?>
-              <p class="event-dates"><?php the_field( 'start_date', 60 )?> - <?php the_field( 'end_date', 60 )?></p>
-              <?php
-            }
+								// Event Address
+								if( get_field('event_address', 60) ): 
+									$location = get_field('event_address', 60);
+									?>
+									<p class="event-address"><?php echo $location['address']; ?></p>
+									<?php 
+								endif; 
+							endif;
+							?>
 
-            // Event Address
-            if( get_field('event_address', 60) ): 
-              $location = get_field('event_address', 60);
-              ?>
-              <p class="event-address"><?php echo $location['address']; ?></p>
-              <?php 
-            endif; 
-					endif;
-					?>
-					
-					<a class="schedule-page-link" href="<?php echo esc_url( get_page_link( 42 ) ) ?>">
-						See Event Schedule
-					</a>
+							<a class="schedule-page-link" href="<?php echo esc_url( get_page_link( 42 ) ) ?>">
+								Event Schedule
+							</a>
+						</div>
+					</div>
 				</div>
 			</section>
 
 			<section class="section-news">
 				<h2>News and Updates</h2>
-				<?php 
-				$args = array(
-					'post_type' => 'post',
-					'posts_per_page' => 3,
-				);
-				
-				$query = new WP_Query( $args );
-				
-				if ( $query -> have_posts() ){
-					while ( $query -> have_posts() ) {
-						$query -> the_post();
-
-						?>
-						<article>
-							<a href=<?php echo get_page_link() ?>>
-								<?php the_post_thumbnail() ?>
+				<div class="section-news-wrapper">
+						<?php 
+					$args = array(
+						'post_type' => 'post',
+						'posts_per_page' => 3,
+					);
+					
+					$query = new WP_Query( $args );
+					
+					if ( $query -> have_posts() ){
+						while ( $query -> have_posts() ) {
+							$query -> the_post();
+							
+							?>
+							<article>
+								<a href=<?php echo get_page_link() ?>>
+									<?php the_post_thumbnail() ?>
+								</a>
 								<h3><?php the_title() ?></h3>
-							</a>
-							<p><?php echo get_the_date() ?></p>
-							<?php the_excerpt() ?>
-						</article>
-						<?php
+								<p class="news-date"><?php echo get_the_date() ?></p>
+								<?php the_excerpt() ?>
+							</article>
+							<?php
+						}
+						wp_reset_postdata();
 					}
-					wp_reset_postdata();
-				}
-				?>
+					?>
+				</div>
 			</section>
 			<?php
 			the_content();
