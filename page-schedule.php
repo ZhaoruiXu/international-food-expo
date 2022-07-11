3feda4a07082d88cb715a711475951a827f6adb2
amrikgrewal <101661032+amrikgrewal@users.noreply.github.com>
2022-07-11 14:00:30 -0700

    news page and schedule page layout change

--- page-schedule.php
+++ page-schedule.php
@@ -35,7 +35,7 @@ get_header();
                      class="ife-events">
                         <?php the_post_thumbnail( 'ife-event-thumbnail') ?>
                         
-                            <h2 class="event-heading"><a href="<?php the_permalink(); ?> "><?php the_title() ?></a></h2>
+                            <h3 class="event-heading"><a href="<?php the_permalink(); ?> "><?php the_title() ?></a></h3>
                             <p class="event-time"><?php the_field('time'); ?></p>
                             <div class="event-description">
                                 <?php the_field('description'); ?>
