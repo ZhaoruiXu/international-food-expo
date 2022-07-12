45d8bd8823ee3e0f5c7e2814d0fc371d0d6eb561
cljang <clayton.jang@gmail.com>
2022-07-12 15:56:46 -0700

    Tweak news page and post styling

--- single.php
+++ single.php
@@ -13,29 +13,24 @@ get_header();
 <main id="primary" class="site-main">
 	
 	<?php
-		while ( have_posts() ) :
-			the_post();
-			get_template_part( 'template-parts/content', get_post_type() );
-			
-			
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'food-expo' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'food-expo' ) . '</span> <span class="nav-title">%title</span>',
-					)
-				);	
-				// If comments are open or we have at least one comment, load up the comment template.
-				// if ( comments_open() || get_comments_number() ) :
-					// 	comments_template();
-					// endif;
+	while ( have_posts() ) :
+		the_post();
+		get_template_part( 'template-parts/content', get_post_type() );
+		
+		
+		the_post_navigation(
+			array(
+				'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'food-expo' ) . '</span> <span class="nav-title">%title</span>',
+				'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'food-expo' ) . '</span> <span class="nav-title">%title</span>',
+			)
+		);
 					
-				endwhile; // End of the loop.
-				?>
+	endwhile; // End of the loop.
+	?>
 				
-				<?php get_template_part('template-parts/featured-vendors'); ?>
+	<?php get_template_part('template-parts/featured-vendors'); ?>
 				
-			</main><!-- #main -->
+</main><!-- #main -->
 
-				<?php
-				get_sidebar();
-				get_footer();
+<?php
+get_footer();
