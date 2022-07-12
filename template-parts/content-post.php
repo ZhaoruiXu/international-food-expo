0d48080922ae45efde8dfa63a797d264c68365be
cljang <clayton.jang@gmail.com>
2022-07-12 14:24:28 -0700

    Add banner to single posts

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -11,11 +11,11 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php food_expo_post_thumbnail(); ?>
 		<?php
 		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			get_template_part( 'template-parts/banner' );
 		else :
+			food_expo_post_thumbnail();
 			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
 		endif;
 		
