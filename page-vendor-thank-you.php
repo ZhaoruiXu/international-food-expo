771cbc6d029f952b1e160d7b4229fdb25b04a815
cljang <clayton.jang@gmail.com>
2022-07-13 01:12:29 -0700

    Style vendor thank you page

--- page-vendor-thank-you.php
+++ page-vendor-thank-you.php
@@ -28,52 +28,55 @@ get_header();
       if (get_post( $new_post_id ) ) : 
         $new_post = get_post( $new_post_id );
         ?>
-        <section>
-          <p>We will process your application and get back to shortly.</p>
-          <table>
-            <caption>Application Info</caption>
-            <tbody>
-              <?php
-              if(function_exists( 'get_field' )){
-                if(get_field('select_vendor_tier', $new_post_id )){
-                  ?>
-                  <tr><td>Company Name</td><td><?php echo $new_post -> post_title ?></td></tr>
-                  <?php
+        <section class="application-summary-section">
+          <div class="content-wrapper">
+            <h2>Thank you for your application.</h2>
+            <p>We have received your application and will contact you pending our review process to get additional information or to finalize your application.</p>
+            <table class="summary-table">
+              <caption>Application Info</caption>
+              <tbody>
+                <?php
+                if(function_exists( 'get_field' )){
+                  if(get_field('select_vendor_tier', $new_post_id )){
+                    ?>
+                    <tr><td>Company Name</td><td><?php echo $new_post -> post_title ?></td></tr>
+                    <?php
+                  }
+                  if(get_field('company_website', $new_post_id )){
+                    ?>
+                    <tr><td>Company Website</td><td><?php echo get_field('company_website', $new_post_id ) ?></td></tr>
+                    <?php
+                  }
+                  if(get_field('company_description', $new_post_id )){
+                    ?>
+                    <tr><td>Company Description</td><td><?php echo get_field('company_description', $new_post_id ) ?></td></tr>
+                    <?php
+                  }
+                  if(get_field('select_vendor_tier', $new_post_id )){
+                    ?>
+                    <tr><td>Selected Vendor Tier</td><td><?php echo get_field('select_vendor_tier', $new_post_id )->name ?></td></tr>
+                    <?php
+                  }
+                  if(get_field('full_name', $new_post_id )){
+                    ?>
+                    <tr><td>Contact Name</td><td><?php echo get_field('full_name', $new_post_id ) ?></td></tr>
+                    <?php
+                  }
+                  if(get_field('email_address', $new_post_id )){
+                    ?>
+                    <tr><td>Email</td><td><?php echo get_field('email_address', $new_post_id ) ?></td></tr>
+                    <?php
+                  }
+                  if(get_field('phone_number', $new_post_id )){
+                    ?>
+                    <tr><td>Phone Number</td><td><?php echo get_field('phone_number', $new_post_id ) ?></td></tr>
+                    <?php
+                  }
                 }
-                if(get_field('company_website', $new_post_id )){
-                  ?>
-                  <tr><td>Company Website</td><td><?php echo get_field('company_website', $new_post_id ) ?></td></tr>
-                  <?php
-                }
-                if(get_field('company_description', $new_post_id )){
-                  ?>
-                  <tr><td>Company Description</td><td><?php echo get_field('company_description', $new_post_id ) ?></td></tr>
-                  <?php
-                }
-                if(get_field('select_vendor_tier', $new_post_id )){
-                  ?>
-                  <tr><td>Selected Vendor Tier</td><td><?php echo get_field('select_vendor_tier', $new_post_id )->name ?></td></tr>
-                  <?php
-                }
-                if(get_field('full_name', $new_post_id )){
-                  ?>
-                  <tr><td>Contact Name</td><td><?php echo get_field('full_name', $new_post_id ) ?></td></tr>
-                  <?php
-                }
-                if(get_field('email_address', $new_post_id )){
-                  ?>
-                  <tr><td>Email</td><td><?php echo get_field('email_address', $new_post_id ) ?></td></tr>
-                  <?php
-                }
-                if(get_field('phone_number', $new_post_id )){
-                  ?>
-                  <tr><td>Phone Number</td><td><?php echo get_field('phone_number', $new_post_id ) ?></td></tr>
-                  <?php
-                }
-              }
-              ?>
-            </tbody>
-          </table>
+                ?>
+              </tbody>
+            </table>
+          </div>
         </section>
         <?php
       endif;
