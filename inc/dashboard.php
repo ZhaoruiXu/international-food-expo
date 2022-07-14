6b2f658d2c2d26d216a421a691b960b0feb6fcea
cljang <clayton.jang@gmail.com>
2022-07-14 16:13:15 -0700

    Reduce action calls in dashboard setup

--- inc/dashboard.php
+++ inc/dashboard.php
@@ -1,7 +1,7 @@
 <?php 
 
 // Remove all default dashboard widgets
-function ife_remove_all_dashboard_metaboxes() {
+function ife_edit_dashboard_metaboxes() {
   $user = wp_get_current_user();
   // Remove Welcome panel
   remove_action( 'welcome_panel', 'wp_welcome_panel' );
@@ -12,34 +12,23 @@ function ife_remove_all_dashboard_metaboxes() {
   remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
   remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');
   remove_meta_box('dashboard_site_health', 'dashboard', 'normal');
-}
-add_action( 'wp_dashboard_setup', 'ife_remove_all_dashboard_metaboxes' );
 
-// Disable setup widget in WooCommerce
-function disable_woocommerce_setup_remove_dashboard_widgets() {
+  // Disable setup widget in WooCommerce
   remove_meta_box( 'wc_admin_dashboard_setup', 'dashboard', 'normal');
-}
-add_action('wp_dashboard_setup', 'disable_woocommerce_setup_remove_dashboard_widgets', 40);
-
-/**
- * Add a widget to the dashboard.
- *
- * This function is hooked into the 'wp_dashboard_setup' action below.
- */
-function ife_add_dashboard_widgets() {
+  
+  // Add custom widget
   wp_add_dashboard_widget(
       'ife_dashboard_widget',           // Widget slug.
       esc_html__( 'Welcome', 'ife' ),   // Title.
       'ife_dashboard_widget_render'     // Display function.
   ); 
 }
-add_action( 'wp_dashboard_setup', 'ife_add_dashboard_widgets' );
+add_action( 'wp_dashboard_setup', 'ife_edit_dashboard_metaboxes' );
 
 /**
 * Create the function to output the content of our Dashboard Widget.
 */
 function ife_dashboard_widget_render() {
-  // Display whatever you want to show.
   ?>
   <p><?php esc_html_e( "Welcome to the admin view for the International Food Expo website. Use this view to manage the site content.", "ife" ); ?></p>
   <section>
