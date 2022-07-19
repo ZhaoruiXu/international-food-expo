1f934198da254db9fb57242683dcbd4b75443057
Zhaorui Xu <xzr0429@gmail.com>
2022-07-19 13:48:52 -0700

    change Google Maps API key to my own

--- inc/google-api.php
+++ inc/google-api.php
@@ -1,7 +1,7 @@
 <?php 
 
 function my_acf_google_map_api( $api ){
-  $api['key'] = 'AIzaSyBXXzioydGJrV3pWfn5Pe0eYp6iOGyuhco';
+  $api['key'] = 'AIzaSyCPir7Ni3R_oa9ZPOh5i_-_n40zs_Cvw5I';
   return $api;
 }
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
--- js/google-maps-setup.js
+++ js/google-maps-setup.js
@@ -1,7 +1,6 @@
 // Helper Code from https://www.advancedcustomfields.com/resources/google-map/
 jQuery.noConflict();
-(function( $ ) {
-
+(function ($) {
   /**
    * initMap
    *
@@ -13,211 +12,209 @@ jQuery.noConflict();
    * @param   jQuery $el The jQuery element.
    * @return  object The map instance.
    */
-  function initMap( $el ) {
-  
-      // Find marker elements within map.
-      let $markers = $el.find('.marker');
-  
-      // Create gerenic map.
-      let mapArgs = {
-          zoom        : $el.data('zoom') || 16,
-          mapTypeId   : google.maps.MapTypeId.ROADMAP,
-          styles:
-          [
+  function initMap($el) {
+    // Find marker elements within map.
+    let $markers = $el.find(".marker");
+
+    // Create gerenic map.
+    let mapArgs = {
+      zoom: $el.data("zoom") || 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles: [
+        {
+          featureType: "all",
+          elementType: "geometry.fill",
+          stylers: [
             {
-                "featureType": "all",
-                "elementType": "geometry.fill",
-                "stylers": [
-                    {
-                        "weight": "2.00"
-                    }
-                ]
+              weight: "2.00",
+            },
+          ],
+        },
+        {
+          featureType: "all",
+          elementType: "geometry.stroke",
+          stylers: [
+            {
+              color: "#9c9c9c",
+            },
+          ],
+        },
+        {
+          featureType: "all",
+          elementType: "labels.text",
+          stylers: [
+            {
+              visibility: "on",
+            },
+          ],
+        },
+        {
+          featureType: "landscape",
+          elementType: "all",
+          stylers: [
+            {
+              color: "#f2f2f2",
+            },
+          ],
+        },
+        {
+          featureType: "landscape",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#ffffff",
+            },
+          ],
+        },
+        {
+          featureType: "landscape.man_made",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#ffffff",
+            },
+          ],
+        },
+        {
+          featureType: "poi",
+          elementType: "all",
+          stylers: [
+            {
+              visibility: "off",
+            },
+          ],
+        },
+        {
+          featureType: "poi.business",
+          elementType: "labels",
+          stylers: [
+            {
+              visibility: "off",
+            },
+          ],
+        },
+        {
+          featureType: "road",
+          elementType: "all",
+          stylers: [
+            {
+              saturation: -100,
             },
             {
-                "featureType": "all",
-                "elementType": "geometry.stroke",
-                "stylers": [
-                    {
-                        "color": "#9c9c9c"
-                    }
-                ]
+              lightness: 45,
+            },
+          ],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#eeeeee",
+            },
+          ],
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              color: "#7b7b7b",
+            },
+          ],
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.stroke",
+          stylers: [
+            {
+              color: "#ffffff",
+            },
+          ],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "all",
+          stylers: [
+            {
+              visibility: "simplified",
+            },
+          ],
+        },
+        {
+          featureType: "road.arterial",
+          elementType: "labels.icon",
+          stylers: [
+            {
+              visibility: "off",
+            },
+          ],
+        },
+        {
+          featureType: "transit",
+          elementType: "all",
+          stylers: [
+            {
+              visibility: "off",
+            },
+          ],
+        },
+        {
+          featureType: "water",
+          elementType: "all",
+          stylers: [
+            {
+              color: "#46bcec",
             },
             {
-                "featureType": "all",
-                "elementType": "labels.text",
-                "stylers": [
-                    {
-                        "visibility": "on"
-                    }
-                ]
+              visibility: "on",
             },
+          ],
+        },
+        {
+          featureType: "water",
+          elementType: "geometry.fill",
+          stylers: [
             {
-                "featureType": "landscape",
-                "elementType": "all",
-                "stylers": [
-                    {
-                        "color": "#f2f2f2"
-                    }
-                ]
+              color: "#a5cad2",
             },
+          ],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [
             {
-                "featureType": "landscape",
-                "elementType": "geometry.fill",
-                "stylers": [
-                    {
-                        "color": "#ffffff"
-                    }
-                ]
+              color: "#070707",
             },
+          ],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.stroke",
+          stylers: [
             {
-                "featureType": "landscape.man_made",
-                "elementType": "geometry.fill",
-                "stylers": [
-                    {
-                        "color": "#ffffff"
-                    }
-                ]
+              color: "#ffffff",
             },
-            {
-                "featureType": "poi",
-                "elementType": "all",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "poi.business",
-                "elementType": "labels",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "road",
-                "elementType": "all",
-                "stylers": [
-                    {
-                        "saturation": -100
-                    },
-                    {
-                        "lightness": 45
-                    }
-                ]
-            },
-            {
-                "featureType": "road",
-                "elementType": "geometry.fill",
-                "stylers": [
-                    {
-                        "color": "#eeeeee"
-                    }
-                ]
-            },
-            {
-                "featureType": "road",
-                "elementType": "labels.text.fill",
-                "stylers": [
-                    {
-                        "color": "#7b7b7b"
-                    }
-                ]
-            },
-            {
-                "featureType": "road",
-                "elementType": "labels.text.stroke",
-                "stylers": [
-                    {
-                        "color": "#ffffff"
-                    }
-                ]
-            },
-            {
-                "featureType": "road.highway",
-                "elementType": "all",
-                "stylers": [
-                    {
-                        "visibility": "simplified"
-                    }
-                ]
-            },
-            {
-                "featureType": "road.arterial",
-                "elementType": "labels.icon",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "transit",
-                "elementType": "all",
-                "stylers": [
-                    {
-                        "visibility": "off"
-                    }
-                ]
-            },
-            {
-                "featureType": "water",
-                "elementType": "all",
-                "stylers": [
-                    {
-                        "color": "#46bcec"
-                    },
-                    {
-                        "visibility": "on"
-                    }
-                ]
-            },
-            {
-                "featureType": "water",
-                "elementType": "geometry.fill",
-                "stylers": [
-                    {
-                        "color": "#a5cad2"
-                    }
-                ]
-            },
-            {
-                "featureType": "water",
-                "elementType": "labels.text.fill",
-                "stylers": [
-                    {
-                        "color": "#070707"
-                    }
-                ]
-            },
-            {
-                "featureType": "water",
-                "elementType": "labels.text.stroke",
-                "stylers": [
-                    {
-                        "color": "#ffffff"
-                    }
-                ]
-            }
-        ]
-      };
-      let map = new google.maps.Map( $el[0], mapArgs );
-  
-      // Add markers.
-      map.markers = [];
-      $markers.each(function(){
-          initMarker( $(this), map );
-      });
-  
-      // Center map based on markers.
-      centerMap( map );
-  
-      // Return map instance.
-      return map;
+          ],
+        },
+      ],
+    };
+    let map = new google.maps.Map($el[0], mapArgs);
+
+    // Add markers.
+    map.markers = [];
+    $markers.each(function () {
+      initMarker($(this), map);
+    });
+
+    // Center map based on markers.
+    centerMap(map);
+
+    // Return map instance.
+    return map;
   }
-  
+
   /**
    * initMarker
    *
@@ -230,41 +227,39 @@ jQuery.noConflict();
    * @param   object The map instance.
    * @return  object The marker instance.
    */
-  function initMarker( $marker, map ) {
-  
-      // Get position from marker.
-      let lat = $marker.data('lat');
-      let lng = $marker.data('lng');
-      let latLng = {
-          lat: parseFloat( lat ),
-          lng: parseFloat( lng )
-      };
-  
-      // Create marker instance.
-      let marker = new google.maps.Marker({
-          position : latLng,
-          map: map,
-          icon: "https://foodexpo.bcitwebdeveloper.ca/wp-content/themes/food-expo/images/marker.png",
+  function initMarker($marker, map) {
+    // Get position from marker.
+    let lat = $marker.data("lat");
+    let lng = $marker.data("lng");
+    let latLng = {
+      lat: parseFloat(lat),
+      lng: parseFloat(lng),
+    };
+
+    // Create marker instance.
+    let marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+      icon: "https://zhaoruixu.com/food-expo/wp-content/themes/food-expo/images/marker.png",
+    });
+
+    // Append to reference for later use.
+    map.markers.push(marker);
+
+    // If marker contains HTML, add it to an infoWindow.
+    if ($marker.html()) {
+      // Create info window.
+      let infowindow = new google.maps.InfoWindow({
+        content: $marker.html(),
       });
-  
-      // Append to reference for later use.
-      map.markers.push( marker );
-  
-      // If marker contains HTML, add it to an infoWindow.
-      if( $marker.html() ){
-  
-          // Create info window.
-          let infowindow = new google.maps.InfoWindow({
-              content: $marker.html()
-          });
-  
-          // Show info window when marker is clicked.
-          google.maps.event.addListener(marker, 'click', function() {
-              infowindow.open( map, marker );
-          });
-      }
+
+      // Show info window when marker is clicked.
+      google.maps.event.addListener(marker, "click", function () {
+        infowindow.open(map, marker);
+      });
+    }
   }
-  
+
   /**
    * centerMap
    *
@@ -276,32 +271,30 @@ jQuery.noConflict();
    * @param   object The map instance.
    * @return  void
    */
-  function centerMap( map ) {
-  
-      // Create map boundaries from all map markers.
-      let bounds = new google.maps.LatLngBounds();
-      map.markers.forEach(function( marker ){
-          bounds.extend({
-              lat: marker.position.lat(),
-              lng: marker.position.lng()
-          });
+  function centerMap(map) {
+    // Create map boundaries from all map markers.
+    let bounds = new google.maps.LatLngBounds();
+    map.markers.forEach(function (marker) {
+      bounds.extend({
+        lat: marker.position.lat(),
+        lng: marker.position.lng(),
       });
-  
-      // Case: Single marker.
-      if( map.markers.length == 1 ){
-          map.setCenter( bounds.getCenter() );
-  
+    });
+
+    // Case: Single marker.
+    if (map.markers.length == 1) {
+      map.setCenter(bounds.getCenter());
+
       // Case: Multiple markers.
-      } else{
-          map.fitBounds( bounds );
-      }
+    } else {
+      map.fitBounds(bounds);
+    }
   }
-  
+
   // Render maps on page load.
-  $(document).ready(function(){
-      $('.acf-map').each(function(){
-          let map = initMap( $(this) );
-      });
+  $(document).ready(function () {
+    $(".acf-map").each(function () {
+      let map = initMap($(this));
+    });
   });
-  
-  })(jQuery);
+})(jQuery);
